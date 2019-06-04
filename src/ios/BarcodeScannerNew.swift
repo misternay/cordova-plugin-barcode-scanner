3d89d79a81f39db840d9262153e41f39d8119180
misternay <misternay@odds.team>
2019-06-04 15:09:17 +0700

    update source code

--- src/ios/BarcodeScannerNew.swift
+++ src/ios/BarcodeScannerNew.swift
@@ -1,42 +1,59 @@
+import BarcodeScanner
+
 @objc(BarcodeScannerNew) class BarcodeScannerNew : CDVPlugin {
+    var myCallbackId = ""
   @objc(scan:)
   func scan(command: CDVInvokedUrlCommand) {
-    var pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_ERROR
-    )
-
-    let msg = command.arguments[0] as? String ?? ""
-
-    if msg.characters.count > 0 {
-      let toastController: UIAlertController =
-        UIAlertController(
-          title: "",
-          message: msg,
-          preferredStyle: .alert
-        )
-      
-      self.viewController?.present(
-        toastController,
-        animated: true,
-        completion: nil
-      )
-
-      DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-        toastController.dismiss(
-          animated: true,
-          completion: nil
-        )
-      }
-        
-      pluginResult = CDVPluginResult(
-        status: CDVCommandStatus_OK,
-        messageAs: msg
-      )
-    }
-
-    self.commandDelegate!.send(
-      pluginResult,
-      callbackId: command.callbackId
-    )
+    
+    let viewController = makeBarcodeScannerViewController()
+    viewController.title = "Hello Sawasdee"
+    
+    self.viewController?.present(viewController,animated: true,completion: nil)
+    self.myCallbackId = command.callbackId
   }
+
+    private func makeBarcodeScannerViewController() -> BarcodeScannerViewController {
+        let viewController = BarcodeScannerViewController()
+        viewController.codeDelegate = self
+        viewController.errorDelegate = self
+        viewController.dismissalDelegate = self
+        return viewController
+    }
+}
+
+extension BarcodeScannerNew: BarcodeScannerCodeDelegate {
+    func scanner(_ controller: BarcodeScannerViewController, didCaptureCode code: String, type: String) {
+        print("Barcode Data: \(code)")
+        print("Symbology Type: \(type)")
+        
+       let pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK,
+            messageAs: code
+        )
+        
+        self.viewController.dismiss(animated: true) {
+            self.commandDelegate!.send(pluginResult,callbackId: self.myCallbackId)
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+            controller.resetWithError()
+        }
+    }
+}
+
+// MARK: - BarcodeScannerErrorDelegate
+
+extension BarcodeScannerNew: BarcodeScannerErrorDelegate {
+    func scanner(_ controller: BarcodeScannerViewController, didReceiveError error: Error) {
+        print("Barcode Data: \(error)")
+        print(error)
+    }
+}
+
+// MARK: - BarcodeScannerDismissalDelegate
+
+extension BarcodeScannerNew: BarcodeScannerDismissalDelegate {
+    func scannerDidDismiss(_ controller: BarcodeScannerViewController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
 }
